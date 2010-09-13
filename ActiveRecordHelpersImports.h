1b68bd706277624d8b488fa1cfbf5fd4d57766f5
Sangmin Ryu <neocoin@gmail.com>
2010-09-13 12:28:04 -0700

    for onetime importing all ActiveRecordHelpers

---
+++ ActiveRecordHelpersImports.h
@@ -0,0 +1,11 @@
+#ifndef __ACTIVERECORDHELPERSIMPORTS__
+#define __ACTIVERECORDHELPERSIMPORTS__
+
+#import "ActiveRecordHelpers.h"
+#import "NSManagedObject+ActiveRecord.h"
+#import "NSManagedObjectContext+ActiveRecord.h"
+#import "NSManagedObjectModel+ActiveRecord.h"
+#import "NSPersistentStore+ActiveRecord.h"
+#import "NSPersistentStoreCoordinator+ActiveRecord.h"
+
+#endif /* __ACTIVERECORDHELPERSIMPORTS__ */
