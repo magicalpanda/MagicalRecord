467fb071a535c0b23cd5eaaa01961cd66387194a
Saul Mora <saul@casademora.com>
2010-10-01 07:27:31 -0700

    don't need ifdefs when importing headers, only include

--- CoreData+ActiveRecordFetching.h
+++ CoreData+ActiveRecordFetching.h
@@ -1,12 +1,7 @@
 
-#ifndef __ACTIVE_RECORD_FETCHING_FOR_CORE_DATA__
-#define __ACTIVE_RECORD_FETCHING_FOR_CORE_DATA__
-
 #import "ActiveRecordHelpers.h"
 #import "NSManagedObject+ActiveRecord.h"
 #import "NSManagedObjectContext+ActiveRecord.h"
 #import "NSPersistentStoreCoordinator+ActiveRecord.h"
 #import "NSManagedObjectModel+ActiveRecord.h"
 #import "NSPersistentStore+ActiveRecord.h"
-
-#endif
