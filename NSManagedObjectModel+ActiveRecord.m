35eaf46c6ae9770892e29e61a72c49c5a67871d1
Arcank <arcank@free.fr>
2011-06-14 19:23:52 -0700

    Added an early return in -[NSManagedObjectModel+ActiveRecord setDefaultManagedObjectModel:]. If the passed model was the same as the current one, it could have been dealloced in between. (Ref count -1, +1 can pass by 0.)

--- NSManagedObjectModel+ActiveRecord.m
+++ NSManagedObjectModel+ActiveRecord.m
@@ -24,6 +24,7 @@ static NSManagedObjectModel *defaultManagedObjectModel = nil;
 
 + (void) setDefaultManagedObjectModel:(NSManagedObjectModel *)newDefaultModel
 {
+	if (defaultManagedObjectModel == newDefaultModel) return;
 	[defaultManagedObjectModel release];
 	defaultManagedObjectModel = [newDefaultModel retain];
 }
