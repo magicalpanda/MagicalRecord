721ec5377ae375a4c06b75b6ddcd9ae57534a551
Saul Mora <saul@casademora.com>
2011-06-26 16:26:18 -0600

    remove cruft commented code

--- NSPersistentStore+ActiveRecord.m
+++ NSPersistentStore+ActiveRecord.m
@@ -20,15 +20,6 @@ static NSPersistentStore *defaultPersistentStore = nil;
 {
 	[defaultPersistentStore release];
 	defaultPersistentStore = [store retain];
-    
-//#ifdef UIKIT_EXTERN_CLASS
-//    if (store == nil) 
-//    {
-//        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Core Data Error" message:@"The default NSPersistentStore was set to nil. The most likely cause is the NSManagedObjectModel version has changed. Either create a new data store, or delete the previous store to continue" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
-//        [alert show];
-//        [alert release];
-//    }
-//#endif
 }
 
 + (NSString *) directory:(int) type
