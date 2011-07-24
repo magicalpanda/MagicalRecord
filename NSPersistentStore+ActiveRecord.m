b5823ab82ba2e1c2047b00d5e27a03960e793809
Saul Mora <saul@casademora.com>
2011-07-24 16:01:14 -0600

    Fix loading store for iOS

--- NSPersistentStore+ActiveRecord.m
+++ NSPersistentStore+ActiveRecord.m
@@ -34,20 +34,16 @@ static NSPersistentStore *defaultPersistentStore = nil;
 
 + (NSString *)applicationLibraryDirectory
 {
-
-#ifdef TARGET_OS_MAC
+#if !TARGET_OS_IPHONE
         
     NSString *applicationName = [[[NSBundle mainBundle] infoDictionary] valueForKey:(NSString *)kCFBundleNameKey];
     return [[self directory:NSApplicationSupportDirectory] stringByAppendingPathComponent:applicationName];
         
-#elif defined(TARGET_OS_IPHONE)
+#else
 
     return [self directory:NSLibraryDirectory];
         
-#else
-#warning Unsupported OS Target specified
-#endif
-        
+#endif        
 }
 
 + (NSURL *) urlForStoreName:(NSString *)storeFileName
