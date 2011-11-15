7643c5af0504ee90af08a2b85f68b8b28d4c7ddd
Saul Mora <saul@magicalpanda.com>
2011-11-15 12:23:20 -0700

    Re-add fix for cleanup of background queue

--- Source/MRCoreDataAction.m
+++ Source/MRCoreDataAction.m
@@ -30,6 +30,7 @@ void cleanup_save_queue()
 	if (coredata_background_save_queue != NULL)
 	{
 		dispatch_release(coredata_background_save_queue);
+        coredata_background_save_queue = NULL;
 	}
 }
 
