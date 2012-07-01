8bb0d0de7428c376b6561b289eca7fb1a261d05a
Saul Mora <saul@magicalpanda.com>
2012-07-01 14:49:11 -0500

    Persist changes to disk when using: - [NSManagedObjectContext MR_saveInBackgroundErrorHandler:completion:] methods, AND the context is the default context - [MagicalRecord saveInBackground…] methods

--- MagicalRecord/Categories/NSManagedObjectContext/NSManagedObjectContext+MagicalSaves.m
+++ MagicalRecord/Categories/NSManagedObjectContext/NSManagedObjectContext+MagicalSaves.m
@@ -91,21 +91,26 @@
     [self MR_saveInBackgroundErrorHandler:nil completion:completion];
 }
 
+- (void) MR_saveInBackgroundErrorHandler:(void (^)(NSError *))errorCallback;
+{
+    [self MR_saveInBackgroundErrorHandler:errorCallback completion:nil];
+}
+
 - (void) MR_saveInBackgroundErrorHandler:(void (^)(NSError *))errorCallback completion:(void (^)(void))completion;
 {
     [self performBlock:^{
         [self MR_saveWithErrorCallback:errorCallback];
-    
-        if (completion) 
+
+        if (self == [[self class] MR_defaultContext])
+        {
+            [[[self class] MR_rootSavingContext] MR_saveInBackgroundErrorHandler:errorCallback completion:completion];
+        }
+
+        if (self == [[self class] MR_rootSavingContext])
         {
             dispatch_async(dispatch_get_main_queue(), completion);
         }
     }];
-}
-
-- (void) MR_saveInBackgroundErrorHandler:(void (^)(NSError *))errorCallback;
-{
-    [self MR_saveInBackgroundErrorHandler:errorCallback completion:nil];
 }
 
 @end
