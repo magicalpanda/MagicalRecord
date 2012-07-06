f0246e9309f126c80de63546f5cf2a300d69efa8
Jwie <joey.daman@twoup.eu>
2012-07-06 10:14:26 +0200

    [NSManagedObjectContext MR_saveInBackgroundErrorHandler:completion:]
    - Only dispatch completion handler when it's not nil.

--- MagicalRecord/Categories/NSManagedObjectContext/NSManagedObjectContext+MagicalSaves.m
+++ MagicalRecord/Categories/NSManagedObjectContext/NSManagedObjectContext+MagicalSaves.m
@@ -106,7 +106,7 @@
             [[[self class] MR_rootSavingContext] MR_saveInBackgroundErrorHandler:errorCallback completion:completion];
         }
 
-        if (self == [[self class] MR_rootSavingContext])
+        if (completion && self == [[self class] MR_rootSavingContext])
         {
             dispatch_async(dispatch_get_main_queue(), completion);
         }
