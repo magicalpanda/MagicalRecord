5616f1a2215fc1c194a507e3baeeb95c7061ab70
Tony Arnold <tony@thecocoabots.com>
2012-12-13 10:01:02 +1100

    Use the new save methods

--- MagicalRecord/Core/MagicalRecord+Actions.m
+++ MagicalRecord/Core/MagicalRecord+Actions.m
@@ -8,63 +8,127 @@
 #import "CoreData+MagicalRecord.h"
 #import "NSManagedObjectContext+MagicalRecord.h"
 
+
 @implementation MagicalRecord (Actions)
 
-+ (void) saveInBackgroundUsingContext:(NSManagedObjectContext *)localContext block:(void (^)(NSManagedObjectContext *))block completion:(void(^)(void))completion errorHandler:(void(^)(NSError *))errorHandler;
+#pragma mark - Asynchronous saving
+
++ (void) saveWithBlock:(void(^)(NSManagedObjectContext *localContext))block;
 {
-    [localContext performBlock: ^{
-        block(localContext);
-        
-        [localContext MR_saveNestedContextsErrorHandler:errorHandler completion:completion];
-    }];
+    [self saveWithBlock:block completion:nil];
 }
 
-+ (void) saveInBackgroundWithBlock:(void (^)(NSManagedObjectContext *))block completion:(void (^)(void))completion errorHandler:(void (^)(NSError *))errorHandler;
++ (void) saveWithBlock:(void(^)(NSManagedObjectContext *localContext))block completion:(MRSaveCompletionHandler)completion;
 {
     NSManagedObjectContext *mainContext  = [NSManagedObjectContext MR_defaultContext];
     NSManagedObjectContext *localContext = [NSManagedObjectContext MR_contextWithParent:mainContext];
-    
-    [self saveInBackgroundUsingContext:localContext block:block completion:completion errorHandler:errorHandler];
+
+    [localContext performBlock:^{
+        if (block) {
+            block(localContext);
+        }
+
+        [localContext MR_saveSelfAndParentContextsWithCompletion:completion];
+    }];
+}
+
++ (void) saveUsingCurrentContextWithBlock:(void (^)(NSManagedObjectContext *))block completion:(MRSaveCompletionHandler)completion;
+{
+    NSManagedObjectContext *localContext = [NSManagedObjectContext MR_contextForCurrentThread];
+
+    [localContext performBlock:^{
+        if (block) {
+            block(localContext);
+        }
+
+        [localContext MR_saveSelfAndParentContextsWithCompletion:completion];
+    }];
+}
+
+
+#pragma mark - Synchronous saving
+
++ (void) saveWithBlockAndWait:(void(^)(NSManagedObjectContext *localContext))block;
+{
+    NSManagedObjectContext *mainContext  = [NSManagedObjectContext MR_defaultContext];
+    NSManagedObjectContext *localContext = [NSManagedObjectContext MR_contextWithParent:mainContext];
+
+    [localContext performBlockAndWait:^{
+        if (block) {
+            block(localContext);
+        }
+
+        [localContext MR_saveSelfAndParentContextsAndWait];
+    }];
+}
+
++ (void) saveUsingCurrentContextWithBlockAndWait:(void (^)(NSManagedObjectContext *))block;
+{
+    NSManagedObjectContext *localContext = [NSManagedObjectContext MR_contextForCurrentThread];
+
+    [localContext performBlockAndWait:^{
+        if (block) {
+            block(localContext);
+        }
+
+        [localContext MR_saveSelfAndParentContextsAndWait];
+    }];
+}
+
+
+#pragma mark - Deprecated methods
+
++ (void) saveInBackgroundWithBlock:(void(^)(NSManagedObjectContext *localContext))block
+{
+    [[self class] saveWithBlock:block completion:nil];
+}
+
++ (void) saveInBackgroundWithBlock:(void(^)(NSManagedObjectContext *localContext))block completion:(void(^)(void))completion
+{
+    NSManagedObjectContext *mainContext  = [NSManagedObjectContext MR_defaultContext];
+    NSManagedObjectContext *localContext = [NSManagedObjectContext MR_contextWithParent:mainContext];
+
+    [localContext performBlock:^{
+        if (block)
+        {
+            block(localContext);
+        }
+
+        [localContext MR_saveSelfAndParentContextsAndWait];
+
+        if (completion)
+        {
+            completion();
+        }
+    }];
 }
 
 + (void) saveInBackgroundUsingCurrentContextWithBlock:(void (^)(NSManagedObjectContext *))block completion:(void (^)(void))completion errorHandler:(void (^)(NSError *))errorHandler;
 {
     NSManagedObjectContext *localContext = [NSManagedObjectContext MR_contextForCurrentThread];
-    
-    [self saveInBackgroundUsingContext:localContext block:block completion:completion errorHandler:errorHandler];
-}
-                                    
-+ (void) saveWithBlock:(void (^)(NSManagedObjectContext *localContext))block completion:(void (^)(void))completion errorHandler:(void (^)(NSError *))errorHandler;
-{
-    NSManagedObjectContext *mainContext  = [NSManagedObjectContext MR_defaultContext];
-    NSManagedObjectContext *localContext = [NSManagedObjectContext MR_contextWithParent:mainContext];
 
-    block(localContext);
-    
-    if ([localContext hasChanges]) 
-    {
-        [localContext MR_saveWithErrorCallback:errorHandler];
-    }
-    
-    if (completion)
-    {
-        dispatch_async(dispatch_get_main_queue(), completion);
-    }
-}
+    [localContext performBlock:^{
+        if (block) {
+            block(localContext);
+        }
 
-+ (void) saveWithBlock:(void(^)(NSManagedObjectContext *localContext))block
-{   
-    [self saveWithBlock:block completion:nil errorHandler:nil];
-}
+        [localContext MR_saveSelfAndParentContextsWithCompletion:^(BOOL success, NSError *error) {
 
-+ (void) saveInBackgroundWithBlock:(void(^)(NSManagedObjectContext *localContext))block
-{
-    [self saveInBackgroundWithBlock:block completion:nil errorHandler:nil];
-}
-
-+ (void) saveInBackgroundWithBlock:(void(^)(NSManagedObjectContext *localContext))block completion:(void(^)(void))callback
-{
-    [self saveInBackgroundWithBlock:block completion:callback errorHandler:nil];
+            if (success) {
+                if (completion)
+                {
+                    completion();
+                }
+            }
+            else
+            {
+                if (errorHandler)
+                {
+                    errorHandler(error);
+                }
+            }
+        }];
+    }];
 }
 
 @end
