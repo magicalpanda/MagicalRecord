bd88caaacd14a7040a0f3327a0065e07f8684b23
Tony Arnold <tony@thecocoabots.com>
2012-12-14 12:03:12 +1100

    Add Kiwi for saner asynchronous testing

---
+++ Project Files/Mac Unit Tests/NSManagedObjectContext+MagicalSavesSpec.m
@@ -0,0 +1,31 @@
+#import "Kiwi.h"
+
+// Project
+#import "NSManagedObjectContext_MagicalSaves.h"
+
+SPEC_BEGIN(NSManagedObjectContext_MagicalSavesSpec)
+
+describe(@"NSManagedObjectContext_MagicalSaves", ^{
+	beforeAll(^{
+		// Occurs once
+	});
+	
+	afterAll(^{
+		// Occurs once
+	});
+	
+	beforeEach(^{
+		// Occurs before each enclosed "it" block
+	});
+	
+	afterEach(^{
+		// Occurs after each enclosed "it" block
+	});
+	
+	it(@"should really test something", ^{
+		// Test something
+		pending(@" — sadly, you've only just created this spec, so it's currently unimplemented", nil);
+	});
+});
+
+SPEC_END
