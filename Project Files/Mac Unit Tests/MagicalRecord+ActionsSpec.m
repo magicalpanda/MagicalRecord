55af799d60fff099430faf0aca4567e049640081
Tony Arnold <tony@thecocoabots.com>
2012-12-16 18:39:05 +1100

    Add Kiwi for saner asynchronous testing and remove existing GHUnit tests for save methods

---
+++ Project Files/Mac Unit Tests/MagicalRecord+ActionsSpec.m
@@ -0,0 +1,31 @@
+#import "Kiwi.h"
+
+// Project
+#import "MagicalRecord_Actions.h"
+
+SPEC_BEGIN(MagicalRecord_ActionsSpec)
+
+describe(@"MagicalRecord_Actions", ^{
+	beforeAll(^{
+		// Occurs once
+	});
+	
+	afterAll(^{
+		// Occurs once
+	});
+	
+	beforeEach(^{
+		// Occurs before each enclosed "it" block
+	});
+	
+	afterEach(^{
+		// Occurs after each enclosed "it" block
+	});
+	
+	it(@"should really test something", ^{
+		// Test something
+		pending(@" — sadly, you've only just created this spec, so it's currently unimplemented", nil);
+	});
+});
+
+SPEC_END
