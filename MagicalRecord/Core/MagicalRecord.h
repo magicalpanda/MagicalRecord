4fb07d06b16a48fc9e6866bf58bd4b23f4a15e30
Peter Simmons <cabbiepete@gmail.com>
2013-06-05 10:52:20 +1200

    Update CocoaLumberJack integration to define ddLogLevel as a const int rather than int
    [CocoaLumberJack docs](https://github.com/robbiehanson/CocoaLumberjack/wiki/XcodeTricks) suggest setting ddLogLevel as below
    
    ```objective-c
    #ifdef DEBUG
      static const int ddLogLevel = LOG_LEVEL_VERBOSE;
    #else
      static const int ddLogLevel = LOG_LEVEL_WARN;
    #endif
    ```
    
    Which causes an issue in MagicalRecord.h of 
    
    ```
    MagicalRecord/MagicalRecord.h:26:20: Redefinition of 'ddLogLevel' with a different type: 'int' vs 'const int'
    ```
    
    This change fixes that.

--- MagicalRecord/Core/MagicalRecord.h
+++ MagicalRecord/Core/MagicalRecord.h
@@ -23,7 +23,7 @@
 #if MR_ENABLE_ACTIVE_RECORD_LOGGING != 0
       // First, check if we can use Cocoalumberjack for logging
     #ifdef LOG_VERBOSE
-        extern int ddLogLevel;
+        extern const int ddLogLevel;
         #define MRLog(...)  DDLogVerbose(__VA_ARGS__)
     #else
         #define MRLog(...) NSLog(@"%s(%p) %@", __PRETTY_FUNCTION__, self, [NSString stringWithFormat:__VA_ARGS__])
