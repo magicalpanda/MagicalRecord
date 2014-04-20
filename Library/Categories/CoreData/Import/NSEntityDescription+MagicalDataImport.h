6228da3b533d3d04556b09f29da62540450332b8
Tony Arnold <tony@thecocoabots.com>
2014-04-20 22:39:29 +1000

    Add documentation for NSEntityDescription+MagicalDataImport.h

--- Library/Categories/CoreData/Import/NSEntityDescription+MagicalDataImport.h
+++ Library/Categories/CoreData/Import/NSEntityDescription+MagicalDataImport.h
@@ -6,21 +6,35 @@
 //  Copyright 2011 Magical Panda Software LLC. All rights reserved.
 //
 
-
-@interface NSEntityDescription (MagicalRecord_DataImport)
-
-- (NSAttributeDescription *) MR_primaryAttributeToRelateBy;
-- (NSManagedObject *) MR_createInstanceInContext:(NSManagedObjectContext *)context;
+@interface NSEntityDescription (MagicalDataImport)
 
 /**
- *	Safely returns an attribute description for the given name, otherwise returns nil. In certain circumstances, the keys of the dictionary returned by `attributesByName` are not standard NSStrings and won't match using object subscripting or standard `objectForKey:` lookups.
- *
- *  There may be performance implications to using this method if your entity has hundreds or thousands of attributes.
- *
- *	@param	name	Name of the attribute description in the `attributesByName` dictionary on this instance
- *
- *	@return	The attribute description for the given name, otherwise nil
+ * @name Managed Object Creation
  */
-- (NSAttributeDescription *) MR_attributeDescriptionForName:(NSString *)name;
+
+/**
+ * @abstract Creates and returns a new NSManagedObject in the supplied NSManagedObject Context using the current entity description.
+ * @param context Managed Object Context
+ * @return Newly inserted managed object
+ */
+- (NSManagedObject *)MR_createInstanceInContext:(NSManagedObjectContext *)context;
+
+/**
+ * @name Attribute Description Retrieval
+ */
+
+/**
+ * @abstract Safely returns an attribute description for the given name, otherwise returns nil. In certain circumstances, the keys of the dictionary returned by `attributesByName` are not standard NSStrings and won't match using object subscripting or standard `objectForKey:` lookups.
+ * @discussion There may be performance implications when using this method if your entity has a large number of attributes (greater than 100).
+ * @param name Name of the attribute description in the `attributesByName` dictionary on this instance
+ * @return The attribute description for the given name, or nil if it could not be found
+ */
+- (NSAttributeDescription *)MR_attributeDescriptionForName:(NSString *)name;
+
+/**
+ * @abstract Safely returns an attribute description for the value of the "relatedByAttribute" set in the data model on this entity.
+ * @return The attribute description for the provided "relatedByAttribute", or nil if it could not be found
+ */
+- (NSAttributeDescription *)MR_primaryAttributeToRelateBy;
 
 @end
--- Library/Categories/CoreData/Import/NSEntityDescription+MagicalDataImport.m
+++ Library/Categories/CoreData/Import/NSEntityDescription+MagicalDataImport.m
@@ -9,20 +9,13 @@
 #import "MagicalRecord.h"
 #import "NSEntityDescription+MagicalDataImport.h"
 
-@implementation NSEntityDescription (MagicalRecord_DataImport)
-
-- (NSAttributeDescription *) MR_primaryAttributeToRelateBy;
-{
-    NSString *lookupKey = [[self userInfo] valueForKey:kMagicalRecordImportRelationshipLinkedByKey] ?: MR_primaryKeyNameFromString([self name]);
-
-    return [self MR_attributeDescriptionForName:lookupKey];
-}
+@implementation NSEntityDescription (MagicalDataImport)
 
 - (NSManagedObject *) MR_createInstanceInContext:(NSManagedObjectContext *)context;
 {
     Class relatedClass = NSClassFromString([self managedObjectClassName]);
     NSManagedObject *newInstance = [relatedClass MR_createInContext:context];
-   
+
     return newInstance;
 }
 
@@ -45,6 +38,13 @@
     }];
 
     return description;
+}
+
+- (NSAttributeDescription *) MR_primaryAttributeToRelateBy;
+{
+    NSString *lookupKey = [[self userInfo] valueForKey:kMagicalRecordImportRelationshipLinkedByKey] ?: MRPrimaryKeyNameFromString([self name]);
+
+    return [self MR_attributeDescriptionForName:lookupKey];
 }
 
 @end
