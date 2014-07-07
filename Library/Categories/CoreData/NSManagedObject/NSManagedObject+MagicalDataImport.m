1a6dfaba70a48538ab281e980f5c58b5f3f6e5ce
Maxim Malygin <Maxim.Malygin@dataart.com>
2014-07-07 12:23:25 +0300

    Data import to ordered relationship was fixed

--- Library/Categories/CoreData/NSManagedObject/NSManagedObject+MagicalDataImport.m
+++ Library/Categories/CoreData/NSManagedObject/NSManagedObject+MagicalDataImport.m
@@ -104,7 +104,10 @@ NSString * const kMagicalRecordImportAttributeUseDefaultValueWhenNotPresent = @"
             NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[self methodSignatureForSelector:selector]];
             [invocation setSelector:selector];
             [invocation invokeWithTarget:self];
-            [invocation getReturnValue:&relationshipSource];
+            
+            __unsafe_unretained id orderedSet;
+            [invocation getReturnValue:&orderedSet];
+            relationshipSource = orderedSet;
 
             addRelationMessageFormat = @"addObject:";
         }
@@ -116,7 +119,7 @@ NSString * const kMagicalRecordImportAttributeUseDefaultValueWhenNotPresent = @"
     
     @try 
     {
-        NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[self methodSignatureForSelector:selector]];
+        NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:[relationshipSource methodSignatureForSelector:selector]];
         [invocation setSelector:selector];
         [invocation setArgument:&relatedObject atIndex:2];
         [invocation invokeWithTarget:relationshipSource];
