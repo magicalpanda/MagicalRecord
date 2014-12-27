3be8978deb3d5d65f669d86f33387e5302969c4e
Tony Arnold <tony@thecocoabots.com>
2014-12-27 21:33:35 +1100

    Remove unused methods

--- Tests/Core/NSManagedObjectContext+ChainSaveTests.m
+++ Tests/Core/NSManagedObjectContext+ChainSaveTests.m
@@ -16,16 +16,6 @@
 
 @implementation NSManagedObjectContext_ChainSaveTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
 - (void)testChainSave {
     //Test if a new Object can save from child context to parent context
     __block NSManagedObjectID *childObjectID;
