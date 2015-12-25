0b04468bbdc187491a76ec5530f0e40d61d1b446
Andrey Mikhaylov <and.mikhaylov@gmail.com>
2015-12-25 14:44:12 +0100

    Add __nullable attribute on groupingKeyPath
    As per docs sectionNameKeyPath can be nil to indicate that NSFetchedResultsController should generate a single section.

--- Library/Categories/CoreData/NSManagedObject/NSManagedObject+MagicalFetching.h
+++ Library/Categories/CoreData/NSManagedObject/NSManagedObject+MagicalFetching.h
@@ -18,25 +18,25 @@ NS_ASSUME_NONNULL_BEGIN
 
 #if TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR
 
-+ (NSFetchedResultsController *)MR_fetchController:(NSFetchRequest *)request delegate:(id<NSFetchedResultsControllerDelegate> __nullable)delegate useFileCache:(BOOL)useFileCache groupedBy:(NSString *)groupKeyPath inContext:(NSManagedObjectContext *)context;
++ (NSFetchedResultsController *)MR_fetchController:(NSFetchRequest *)request delegate:(id<NSFetchedResultsControllerDelegate> __nullable)delegate useFileCache:(BOOL)useFileCache groupedBy:(NSString *__nullable)groupKeyPath inContext:(NSManagedObjectContext *)context;
 
 + (NSFetchedResultsController *)MR_fetchAllSortedBy:(NSString *)sortTerm
                                           ascending:(BOOL)ascending
                                       withPredicate:(NSPredicate *__nullable)searchTerm
-                                            groupBy:(NSString *)groupingKeyPath
+                                            groupBy:(NSString *__nullable)groupingKeyPath
                                            delegate:(id<NSFetchedResultsControllerDelegate> __nullable)delegate;
 + (NSFetchedResultsController *)MR_fetchAllSortedBy:(NSString *)sortTerm
                                           ascending:(BOOL)ascending
                                       withPredicate:(NSPredicate *__nullable)searchTerm
-                                            groupBy:(NSString *)groupingKeyPath
+                                            groupBy:(NSString *__nullable)groupingKeyPath
                                            delegate:(id<NSFetchedResultsControllerDelegate> __nullable)delegate
                                           inContext:(NSManagedObjectContext *)context;
 
-+ (NSFetchedResultsController *)MR_fetchAllGroupedBy:(NSString *)group withPredicate:(NSPredicate *__nullable)searchTerm sortedBy:(NSString *)sortTerm ascending:(BOOL)ascending;
-+ (NSFetchedResultsController *)MR_fetchAllGroupedBy:(NSString *)group withPredicate:(NSPredicate *__nullable)searchTerm sortedBy:(NSString *)sortTerm ascending:(BOOL)ascending inContext:(NSManagedObjectContext *)context;
++ (NSFetchedResultsController *)MR_fetchAllGroupedBy:(NSString *__nullable)group withPredicate:(NSPredicate *__nullable)searchTerm sortedBy:(NSString *)sortTerm ascending:(BOOL)ascending;
++ (NSFetchedResultsController *)MR_fetchAllGroupedBy:(NSString *__nullable)group withPredicate:(NSPredicate *__nullable)searchTerm sortedBy:(NSString *)sortTerm ascending:(BOOL)ascending inContext:(NSManagedObjectContext *)context;
 
-+ (NSFetchedResultsController *)MR_fetchAllGroupedBy:(NSString *)group withPredicate:(NSPredicate *__nullable)searchTerm sortedBy:(NSString *)sortTerm ascending:(BOOL)ascending delegate:(id<NSFetchedResultsControllerDelegate> __nullable)delegate;
-+ (NSFetchedResultsController *)MR_fetchAllGroupedBy:(NSString *)group withPredicate:(NSPredicate *__nullable)searchTerm sortedBy:(NSString *)sortTerm ascending:(BOOL)ascending delegate:(id<NSFetchedResultsControllerDelegate> __nullable)delegate inContext:(NSManagedObjectContext *)context;
++ (NSFetchedResultsController *)MR_fetchAllGroupedBy:(NSString *__nullable)group withPredicate:(NSPredicate *__nullable)searchTerm sortedBy:(NSString *)sortTerm ascending:(BOOL)ascending delegate:(id<NSFetchedResultsControllerDelegate> __nullable)delegate;
++ (NSFetchedResultsController *)MR_fetchAllGroupedBy:(NSString *__nullable)group withPredicate:(NSPredicate *__nullable)searchTerm sortedBy:(NSString *)sortTerm ascending:(BOOL)ascending delegate:(id<NSFetchedResultsControllerDelegate> __nullable)delegate inContext:(NSManagedObjectContext *)context;
 
 #endif
 
