f5f427b8b1653f921eb7de8bd843bfec34fd5334
Tony Arnold <tony@thecocoabots.com>
2017-03-15 22:25:54 +1100

    Ensure NSManagedObject+MagicalFetching.h is in the framework umbrella header

--- Library/MagicalRecord.h
+++ Library/MagicalRecord.h
@@ -34,6 +34,7 @@
 
 #import "NSManagedObject+MagicalRecord.h"
 #import "NSManagedObject+MagicalRequests.h"
+#import "NSManagedObject+MagicalFetching.h"
 #import "NSManagedObject+MagicalFinders.h"
 #import "NSManagedObject+MagicalAggregation.h"
 #import "NSManagedObjectContext+MagicalRecord.h"
