4c5a341c2c50c98a7f8b9336ccdb031dcbfa0b7b
ligang <ligang@zepplabs.com>
2017-10-27 13:40:49 +0800

    Add support for watch os.

--- MagicalRecord/Categories/NSManagedObjectContext/NSManagedObjectContext+MagicalObserving.m
+++ MagicalRecord/Categories/NSManagedObjectContext/NSManagedObjectContext+MagicalObserving.m
@@ -85,24 +85,28 @@ NSString * const kMagicalRecordDidMergeChangesFromiCloudNotification = @"kMagica
 	}
 }
 
+#if !TARGET_OS_WATCH
+
 - (void) MR_observeiCloudChangesInCoordinator:(NSPersistentStoreCoordinator *)coordinator;
 {
-    if (![MagicalRecord isICloudEnabled]) return;
-    NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
-    [notificationCenter addObserver:self
-                           selector:@selector(MR_mergeChangesFromiCloud:)
-                               name:NSPersistentStoreDidImportUbiquitousContentChangesNotification
-                             object:coordinator];
-    
+	if (![MagicalRecord isICloudEnabled]) return;
+	NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
+	[notificationCenter addObserver:self
+						   selector:@selector(MR_mergeChangesFromiCloud:)
+							   name:NSPersistentStoreDidImportUbiquitousContentChangesNotification
+							 object:coordinator];
+	
 }
 
 - (void) MR_stopObservingiCloudChangesInCoordinator:(NSPersistentStoreCoordinator *)coordinator;
 {
-    if (![MagicalRecord isICloudEnabled]) return;
-    NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
-    [notificationCenter removeObserver:self
-                                  name:NSPersistentStoreDidImportUbiquitousContentChangesNotification
-                                object:coordinator];
+	if (![MagicalRecord isICloudEnabled]) return;
+	NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
+	[notificationCenter removeObserver:self
+								  name:NSPersistentStoreDidImportUbiquitousContentChangesNotification
+								object:coordinator];
 }
+
+#endif
 
 @end
