65ff6ec4baa1501b4f99d9d0992b9dc999f36166
Nick Servidio <servidionicholas@gmail.com>
2019-03-19 17:38:03 -0400

    Hard coding versions.

--- Support/Scripts/set_version_information.sh
+++ Support/Scripts/set_version_information.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 git=$(sh /etc/profile; which git)
-number_of_commits=$("$git" rev-list HEAD --count)
-git_release_version=$("$git" describe --tags --always --abbrev=0)
+number_of_commits="7442"
+git_release_version="2.3.3"
 
 target_plist="$TARGET_BUILD_DIR/$INFOPLIST_PATH"
 dsym_plist="$DWARF_DSYM_FOLDER_PATH/$DWARF_DSYM_FILE_NAME/Contents/Info.plist"
