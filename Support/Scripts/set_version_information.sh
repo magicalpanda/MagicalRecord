13feda8ef363dea4e2fd3178672215baa108611b
Matheus Oliveira Costa <matheusoliveiracos@gmail.com>
2020-09-15 09:16:32 -0300

    Fixes issue when compiling using carthage and using repository tag

--- Support/Scripts/set_version_information.sh
+++ Support/Scripts/set_version_information.sh
@@ -3,6 +3,7 @@
 git=$(sh /etc/profile; which git)
 number_of_commits=$("$git" rev-list HEAD --count)
 git_release_version=$("$git" describe --tags --always --abbrev=0)
+clear_release_version=$(echo $git_release_version | grep -o -E '[0-9]+')
 
 target_plist="$TARGET_BUILD_DIR/$INFOPLIST_PATH"
 dsym_plist="$DWARF_DSYM_FOLDER_PATH/$DWARF_DSYM_FILE_NAME/Contents/Info.plist"
@@ -10,6 +11,6 @@ dsym_plist="$DWARF_DSYM_FOLDER_PATH/$DWARF_DSYM_FILE_NAME/Contents/Info.plist"
 for plist in "$target_plist" "$dsym_plist"; do
   if [ -f "$plist" ]; then
     /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $number_of_commits" "$plist"
-    /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${git_release_version#*v}" "$plist"
+    /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${clear_release_version#*v}" "$plist"
   fi
 done
